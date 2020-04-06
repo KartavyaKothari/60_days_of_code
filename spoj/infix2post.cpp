cbdf4460180d972a518468f9f6f0fe99f2ade33d
KARTAVYA PRAVIN KOTHARI <kartavya@cse.iitb.ac.in>
2020-04-06 11:03:34 +0530

    Working implementation of infix to postfix

--- spoj/infix2post.cpp
+++ spoj/infix2post.cpp
@@ -2,24 +2,24 @@
 
 using namespace std;
 
-enum Priority {power,division,mul,sub,add};
+enum Priority {add,sub,mul,division,power};
 stack<char> operands;
 
 int priority(char op){
     switch(op){
-        case '^':return power;
-        case '/':return division;
-        case '*':return mul;
-        case '-':return sub;
         case '+':return add;
+        case '-':return sub;
+        case '*':return mul;
+        case '/':return division;
+        case '^':return power;
     }
 }
 
-void solve(){
+void solve(string exp){
     char op_old;
     char op_new;
-    string exp;
-    cin>>exp;
+    // string exp;
+    // cin>>exp;
     // vector<char> infix(exp.begin(),exp.end());
 
     for(auto i : exp){
@@ -29,38 +29,44 @@ void solve(){
             operands.push('(');
             continue;
         }
+        
         if(i==')'){
-            while(true){
-                if(operands.empty())break;
+            while(!operands.empty()){
                 if(operands.top()=='('){operands.pop();break;}
                 cout<<operands.top();
                 operands.pop();
             }
             continue;
         }
+        
         if(isalpha(op_new)){
+            // cout<<"[print "<<op_new<<"]";
             cout<<op_new;
-        }else{
-            if(operands.empty()){
-                operands.push(op_new);
-            }else{
-                op_old = operands.top();
-                if(priority(op_new)<priority(op_old)){
-                    operands.push(op_new);
-                }else{
-                    while(priority(op_new)>=priority(op_old)){
-                        cout<<operands.top();
-                        operands.pop();
-                        if(operands.empty())break;
-                        op_old = operands.top();
-                    }
-                    operands.push(op_new);
-                }
-            }
+            continue;
         }
+
+        if(operands.empty()){
+            // cout<<"[Pushing "<<op_new<<"]";
+            operands.push(op_new);
+            continue;
+        }
+
+        op_old = operands.top();
+        
+        while(priority(op_new)<priority(op_old)){
+            // cout<<"["<<op_new<<" lower than "<<op_old<<" popping "<<op_old<<"]";
+            cout<<operands.top();
+            operands.pop();
+            if(operands.empty())break;
+            op_old = operands.top();
+        }
+
+        // cout<<"[ pushing "<<op_new<<"]";
+        operands.push(op_new);
     }
 
     while(!operands.empty()){
+        // cout<<"[Popping .. "<<operands.top()<<"]";
         cout<<operands.top();
         operands.pop();
     }
@@ -71,9 +77,16 @@ int main(int argc, char const *argv[])
     int n;
 
     cin>>n;
+    vector<string> exps;
+    string temp;
 
-    while(n--){
-        solve();
+    for(int i = 0 ; i < n ; i++){
+        cin>>temp;
+        exps.push_back(temp);
+    }
+
+    for(auto i : exps){
+        solve(i);
         cout<<endl;
     }
 
