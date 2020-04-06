8c7e332f300cf3cd0672f52b4331eaebc03360a5
KARTAVYA PRAVIN KOTHARI <kartavya@cse.iitb.ac.in>
2020-04-06 22:57:37 +0530

    Resolved the TLE error, the code is accepted

--- spoj/thenextpalindrome.cpp
+++ spoj/thenextpalindrome.cpp
@@ -7,12 +7,10 @@ bool isPalin(string str){
 
 string incre_string(string s){
     bool carry = true;
-    char a;
     int num;
 
     for(auto i = s.rbegin() ; carry && i != s.rend() ; i++){
-        a = *i;
-        num = atoi(&a);
+        num = *i-'0';
         if(++num>9){
             *i='0';
         }else{
@@ -22,10 +20,77 @@ string incre_string(string s){
     }
 
     if(carry){
-        s='1'+s;
+        return '1'+s;
+    } else return s;
+}
+
+bool isOdd(string input){
+    if(input.length() % 2 == 0 ) return false;
+    else return true;
+}
+
+bool isAllNines(string input){
+    for(auto i = input.begin() ; i != input.end() ; i++){
+        if(*i=='9'){
+            continue;
+        }else{
+            return false;
+        }
     }
 
-    return s;
+    return true;
+}
+
+void solveBetter(string input){
+    string firstHalf = string(input.begin(),input.begin() + (input.length()-1)/2+1);
+
+    if(isAllNines(input)){
+        cout<<'1';
+        for(int i = 0 ; i < input.length() -1 ; i++){
+            cout<<'0';
+        }
+        cout<<'1';
+        return;
+    }
+
+    if(isPalin(input)){
+        firstHalf = incre_string(firstHalf);
+
+        if(isOdd(input)){
+            cout<<firstHalf+string(firstHalf.rbegin()+1,firstHalf.rend());
+        }else{
+            cout<<firstHalf+string(firstHalf.rbegin(),firstHalf.rend());
+        }
+    }else{
+        if(isOdd(input)){
+            int i;
+            for(i = 0 ; i < firstHalf.length()-1 ; i++){
+                if(input[firstHalf.length()-1-i]==input[firstHalf.length()-1+i]){
+                    continue;
+                }else break;
+            }
+
+            if(input[firstHalf.length()-1-i]<input[firstHalf.length()-1+i]){
+                firstHalf = incre_string(string(firstHalf.begin(),firstHalf.end()));
+                cout<<firstHalf+string(firstHalf.rbegin()+1,firstHalf.rend());
+            }else{
+                cout<<firstHalf+string(firstHalf.rbegin()+1,firstHalf.rend());
+            }
+        }else{
+            int i;
+            for(i = 0 ; i < firstHalf.length() ; i++){
+                if(input[firstHalf.length()-1-i]==input[firstHalf.length()+i]){
+                    continue;
+                }else break;
+            }
+
+            if(input[firstHalf.length()-1-i]<input[firstHalf.length()+i]){
+                firstHalf = incre_string(firstHalf);
+            }
+
+            cout<<firstHalf+string(firstHalf.rbegin(),firstHalf.rend());
+        }
+    }
 }
 
 void solve(string input){
@@ -48,7 +113,7 @@ int main(int argc, char const *argv[])
     }
 
     for(int i = 0 ; i < k ; i++){
-        solve(input[i]);
+        solveBetter(input[i]);
         cout<<endl;
     }
 
