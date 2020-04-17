1d2e70f4e54de562bef104b85c4425daf4ca6c03
KARTAVYA PRAVIN KOTHARI <kartavya@cse.iitb.ac.in>
2020-04-17 12:47:48 +0530

    Added naive implementation of min-cost-path

---
+++ dynamic_algo_geeks4geeks/min-cost-path.cpp
@@ -0,0 +1,100 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int min(int a, int b, int c){
+    if(a<b){
+        if(a<c){
+            return a;
+        }else return c;
+    }else{
+        if(b<c){
+            return b;
+        }else return c;
+    }
+}
+
+// int min_cost_path(int i , int j, vector<vector<int>> cost_table){
+//     if(j==0){
+//         int sum = 0;
+//         for(int index = i ; index >= 0 ; index--){
+//             sum += cost_table[index][j];
+//         }
+//         return sum;
+//     }
+
+//     if(i==0){
+//         int sum = 0;
+//         for(int index = j ; index >= 0 ; index--){
+//             sum += cost_table[i][index];
+//         }
+//         return sum;
+//     }
+//     // if(i>=cost_table.size()||j>=cost_table[0].size())return 9999;
+    
+//     return cost_table[i][j] + min(
+//         min_cost_path(i-1,j,cost_table),
+//         min_cost_path(i,j-1,cost_table),
+//         min_cost_path(i-1,j-1,cost_table)
+//     );
+// }
+
+int min_cost_path(int i , int j, vector<vector<int>> cost_table){
+    if(j==cost_table[0].size()-1){
+        int sum = 0;
+        for(int index = i ; index < cost_table.size() ; index++){
+            sum += cost_table[index][j];
+        }
+        return sum;
+    }
+
+    if(i==cost_table.size()-1){
+        int sum = 0;
+        for(int index = j ; index < cost_table[0].size() ; index++){
+            sum += cost_table[i][index];
+        }
+        return sum;
+    }
+    // if(i>=cost_table.size()||j>=cost_table[0].size())return 9999;
+    
+    return cost_table[i][j] + min(
+        min_cost_path(i+1,j,cost_table),
+        min_cost_path(i,j+1,cost_table),
+        min_cost_path(i+1,j+1,cost_table)
+    );
+}
+
+void solve(vector<vector<int>> cost_table){
+    cout<<min_cost_path(cost_table.size()-1,cost_table[0].size()-1,cost_table);
+    // for(auto row : cost_table){
+    //     for(auto ele : row){
+    //         cout<<ele<<" ";
+    //     }cout<<endl;
+    // }
+}
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin>>n;
+
+    vector<vector<vector<int>>> cases(n);
+
+    for(int i = 0 ; i < n ; i++){
+        int a,b;
+        cin>>a>>b;
+        vector<vector<int>> vec(a,vector<int>(b,0));
+        for(int j = 0 ; j < a ; j++){
+            for(int k = 0 ; k < b ; k++){
+                cin>>vec[j][k];
+            }
+        }
+        cases[i]=vec;
+    }
+
+    for(int i = 0 ; i < n ; i++){
+        solve(cases[i]);
+        cout<<endl;
+    }
+
+    return 0;
+}
